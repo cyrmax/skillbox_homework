edc10aa5e145a7a050032a7366ad718777b3ad6a
Kirill <belousov.k.m@yandex.ru>
2020-03-31 13:49:03 +0300

    Task about power of number 2

---
+++ CalculateIntPower/CalculateIntPower/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+
+    @IBAction func numberEditingChanged(_ sender: Any) {
+                guard let number = Int(numberTextField.text!) else {
+                    resultLabel.text = "Incorrect data"
+        return
+                }
+                resultLabel.text = calculate(number)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func calculate(_ n: Int) -> String {
+var result = 1
+        for _ in 0..<n {
+result *= 2
+        }
+        return result.description
+    }
+}
+
