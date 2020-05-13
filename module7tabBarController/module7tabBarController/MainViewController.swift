b6ccb0612ae38950ae0ce8a4dc310e9f644ed311
Kirill <belousov.k.m@yandex.ru>
2020-05-13 18:14:41 +0300

    Renamed second tab to third tab as in task

--- module7tabBarController/module7tabBarController/MainViewController.swift
+++ module7tabBarController/module7tabBarController/MainViewController.swift
@@ -11,8 +11,8 @@ let firstNav = UINavigationController(rootViewController: firstTab)
         firstNav.setNavigationBarHidden(false, animated: true)
         firstNav.tabBarItem = UITabBarItem(title: "First", image: nil, tag: 0)
         controllers.append(firstNav)
-let secondTab = SecondTabViewController()
-        secondTab.tabBarItem = UITabBarItem(title: "Second", image: nil, tag: 1)
+let secondTab = ThirdTabViewController()
+        secondTab.tabBarItem = UITabBarItem(title: "Third", image: nil, tag: 2)
         controllers.append(secondTab)
         viewControllers = controllers
     }
