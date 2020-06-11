b7099401fb08a9f1c9ee0030c09dcd971f12e105
Kirill <belousov.k.m@yandex.ru>
2020-06-11 16:56:22 +0300

    Incompleted project with 4 images

---
+++ module8_4images/module8_4images/ViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+class ViewController: UIViewController {
+    var imageArray: [UIImage] = []
+    var imgViews = [UIImageView]()
+var labels = [UILabel]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+                var resourceURL = Bundle.main.resourceURL!
+                resourceURL.appendPathComponent("pics")
+                let fileArray = try! FileManager.default.contentsOfDirectory(at: resourceURL, includingPropertiesForKeys: nil, options: [])
+                for file in fileArray {
+                    imageArray.append(UIImage(contentsOfFile: file.path)!)
+                }
+        setupGraphics()
+    }
+
+    func setupGraphics() {
+        for i in 0...3 {
+let imgView = generateImageView(index: i)
+            view.addSubview(imgView)
+// constraints should be here
+        }
+    }
+
+    func generateImageView(index: Int) -> UIView {
+let imgView = UIImageView(image: imageArray[index])
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+let label = UILabel()
+        label.text = "Label \(index)"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        imgView.addSubview(label)
+        NSLayoutConstraint.activate([
+        label.centerXAnchor.constraint(equalTo: imgView.centerXAnchor),
+        label.centerYAnchor.constraint(equalTo: imgView.centerYAnchor)
+])
+        imgViews.append(imgView)
+        labels.append(label)
+return imgView
+    }
+}
+
