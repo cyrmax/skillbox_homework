12478765dcc27a277c67b9dfaec168ccae95f264
Kirill <belousov.k.m@yandex.ru>
2020-06-14 14:36:21 +0300

    segmented control initial commit

---
+++ segmentedControl/segmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segmentedControl
+//
+//  Created by Kirill on 14.06.2020.
+//  Copyright © 2020 cyrmax-it. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
