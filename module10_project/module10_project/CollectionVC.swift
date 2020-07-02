b398632b034b6cf3dd724409f29d943b108c3392
Cyrmax <belousov.k.m@yandex.ru>
2020-07-03 02:05:45 +0300

    Yet another commit

--- module10_project/module10_project/CollectionVC.swift
+++ module10_project/module10_project/CollectionVC.swift
@@ -5,6 +5,15 @@ class CollectionVC: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+print("Collection controller loaded")
+        var url = Bundle.main.resourceURL!
+        url.appendPathComponent("pics")
+        let fileArray = try! FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+        for file in fileArray {
+            imageArray.append(UIImage(contentsOfFile: file.path)!)
+        }
+        print("images loaded: \(imageArray.count)")
+
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
---
+++ module10_project/module10_project/MainVC.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class MainVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewControllers?.append(CollectionVC(collectionViewLayout: 
+}
