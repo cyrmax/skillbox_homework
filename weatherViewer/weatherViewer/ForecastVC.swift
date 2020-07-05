49f97a8e2f3b0aa3111aeda166ece55dd7b6450a
Cyrmax <belousov.k.m@yandex.ru>
2020-07-05 21:58:06 +0300

    Yet another commit

--- weatherViewer/weatherViewer/ForecastVC.swift
+++ weatherViewer/weatherViewer/ForecastVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Alamofire
 
 class ForecastVC: UITableViewController {
     var weathers = [WeatherResponse]()
@@ -7,7 +8,28 @@ class ForecastVC: UITableViewController {
         super.viewDidLoad()
         tableView.rowHeight = 70
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+fetchWeather()
         tableView.dataSource = self
+    }
 
+    func fetchWeather() {
+        let params: [String: Any] = [
+            "id": cityID,
+            "cnt": 7,
+            "appid": apiKey
+        ]
+
+
+
+        AF.request("https://api.openweathermap.org/data/2.5/forecast/daily?id=\(cityID)&cnt=7&appid=\(apiKey)").validate().response {
+response in
+            switch response.result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let data):
+print(data)
+
+            }
+        }
     }
 }
