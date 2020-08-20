2ece46ce556cff94607f35c01b2ec15b63cf3ea0
Cyrmax <belousov.k.m@ya.ru>
2020-08-20 14:57:04 +0500

    settings cell struct commit

---
+++ module10_project/module10_project/SettingsCellStruct.swift
@@ -0,0 +1,9 @@
+struct SettingsCellStruct {
+    let label: String
+    let number: Int = 0
+    let needsSwitch: Bool = false
+
+//    init(label: String, needsSwitch: Bool = false, number: Int = 0) {
+//        self.init(label: label, needsSwitch: needsSwitch, number: number)
+//    }
+}
