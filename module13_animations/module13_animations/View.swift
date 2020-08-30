77db93cf451117ba4c9459fbcca84db32f1b6155
Cyrmax <belousov.k.m@ya.ru>
2020-08-30 14:13:54 +0500

    Partly working animations

---
+++ module13_animations/module13_animations/View.swift
@@ -0,0 +1,147 @@
+import UIKit
+
+class RootView: UIView {
+    var guide: UILayoutGuide {
+        self.safeAreaLayoutGuide
+    }
+
+    var squareYConstraint: NSLayoutConstraint!
+    var squareXConstraint: NSLayoutConstraint!
+    var squareWConstraint: NSLayoutConstraint!
+    var squareHConstraint: NSLayoutConstraint!
+
+    let square: UIView = {
+let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .red
+return v
+    }()
+
+    let backBtn: UIButton = {
+let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("Previous", for: .normal)
+        b.backgroundColor = .white
+return b
+    }()
+
+    let nextBtn: UIButton = {
+    let b = UIButton()
+    b.translatesAutoresizingMaskIntoConstraints = false
+    b.setTitle("Next", for: .normal)
+    b.backgroundColor = .white
+    return b
+    }()
+
+    let numLbl: UILabel = {
+let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.backgroundColor = .green
+        lbl.text = "0"
+return lbl
+    }()
+
+    let stack: UIStackView = {
+let st = UIStackView()
+        st.translatesAutoresizingMaskIntoConstraints = false
+        st.axis = .horizontal
+        st.distribution = .fillEqually
+        st.alignment = .fill
+return st
+    }()
+
+    private func setupView() {
+        self.backgroundColor = .black
+addSubview(square)
+        stack.addArrangedSubview(backBtn)
+        stack.addArrangedSubview(numLbl)
+        stack.addArrangedSubview(nextBtn)
+addSubview(stack)
+
+        squareXConstraint = square.centerXAnchor.constraint(equalTo: guide.leftAnchor, constant: (UIScreen.main.bounds.width / 2))
+        squareYConstraint = square.centerYAnchor.constraint(equalTo: guide.topAnchor, constant: (UIScreen.main.bounds.height / 2))
+        squareWConstraint = square.widthAnchor.constraint(equalToConstant: 200)
+        squareHConstraint = square.heightAnchor.constraint(equalToConstant: 200)
+
+        NSLayoutConstraint.activate([
+            stack.leftAnchor.constraint(equalTo: guide.leftAnchor),
+            stack.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: 50),
+            stack.rightAnchor.constraint(equalTo: guide.rightAnchor),
+            stack.heightAnchor.constraint(equalToConstant: 70),
+squareXConstraint,
+squareYConstraint,
+squareWConstraint,
+squareHConstraint
+        ])
+
+        backBtn.addTarget(nil, action: #selector(ViewController.previousAnimation), for: .touchUpInside)
+        nextBtn.addTarget(nil, action: #selector(ViewController.nextAnimation), for: .touchUpInside)
+    }
+
+    func animateColor() {
+        UIView.animate(withDuration: 2, animations: {
+            self.square.backgroundColor = .yellow
+        }, completion: {
+isCompleted in
+            self.square.backgroundColor = .red
+        })
+    }
+
+    func animateMovement() {
+        UIView.animate(withDuration: 2, animations: {
+            self.squareXConstraint.constant = UIScreen.main.bounds.width - 100
+            self.squareYConstraint.constant = 100
+            self.layoutIfNeeded()
+        }, completion: {
+            isCompleted in
+            self.squareXConstraint.constant = UIScreen.main.bounds.width / 2
+            self.squareYConstraint.constant = UIScreen.main.bounds.height / 2
+            self.layoutIfNeeded()
+        })
+    }
+
+    func animateRound() {
+let animation = CABasicAnimation(keyPath: "cornerRadius")
+
+    }
+
+    func animateSpin() {
+        UIView.animate(withDuration: 2, animations: {
+            self.square.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        }, completion: {
+            isCompleted in
+            self.square.transform = CGAffineTransform(rotationAngle: 0)
+        })
+    }
+
+    func animateDisappear() {
+        UIView.animate(withDuration: 2, animations: {
+            self.square.alpha = 0
+        }, completion: {
+            isCompleted in
+            self.square.alpha = 1
+        })
+    }
+
+    func animateScale() {
+        UIView.animate(withDuration: 2, delay: 0, options: [.autoreverse], animations: {
+            self.square.transform = CGAffineTransform(scaleX: 2, y: 2)
+        })
+    }
+
+    func animateRotation() {
+        UIView.animate(withDuration: 2, delay: 0, options: [.repeat], animations: {
+            self.square.transform = CGAffineTransform(rotationAngle: 2 * CGFloat.pi)
+        })
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+}
--- module13_animations/module13_animations/ViewController.swift
+++ module13_animations/module13_animations/ViewController.swift
@@ -1,20 +1,21 @@
-//
-//  ViewController.swift
-//  module13_animations
-//
-//  Created by Kirill Belousov on 30.08.2020.
-//  Copyright © 2020 Kirill Belousov. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        self.view = RootView()
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    @objc func previousAnimation() {
+
+    }
+
+    @objc func nextAnimation() {
+        (view as! RootView).animateScale()
+    }
 }
 
