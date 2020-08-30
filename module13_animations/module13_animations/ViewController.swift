ed9e0abccf67e1c20f999c61b28642387783f1c4
Cyrmax <belousov.k.m@ya.ru>
2020-08-30 12:27:07 +0500

    Initial commit for module13

---
+++ module13_animations/module13_animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  module13_animations
+//
+//  Created by Kirill Belousov on 30.08.2020.
+//  Copyright © 2020 Kirill Belousov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
